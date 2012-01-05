794e863022f3f20034a3190c49b39906e51c8348
Marcelo C. de Freitas <marcelo@kow.com.br>
2012-01-05 14:39:39 -0200

    getting error codes is OK and getting types also

--- configure-mysql.sh
+++ configure-mysql.sh
@@ -30,6 +30,7 @@ test_is_set(){
 ###################
 
 
+# Field Types #
 
 generate_field_type_c_chunk(){
 	HDRFILE="${MYSQL_INCLUDE_PATH}/mysql_com.h"
@@ -50,9 +51,35 @@ get_field_type_codes(){
 }
 
 
+
+# Error Codes #
+
+
+
+generate_error_code_c_chunk(){
+	HDR1="${MYSQL_INCLUDE_PATH}/errmsg.h"
+	HDR2="${MYSQL_INCLUDE_PATH}/mysqld_error.h"
+
+	cat "$HDR1" "$HDR2" \
+		| grep -v ERROR_LAST \
+		| grep -v ERROR_FIRST \
+		| sed -n '/^# *define *CR_[A-Za-z0-9_]* */p;/^# *define *ER_[A-Za-z0-9_]* */p' \
+		| sed 's|/\*.*\*/||g;s| *$||' \
+		| sed 's|# *define *||' \
+		| while read NAME VALUE ; do
+			echo "	{ \"$NAME\", $NAME },"
+		done
+		echo "#ifndef CR_NO_ERROR"
+		echo "	{ \"CR_NO_ERROR\", 0 },"
+		echo "#endif"
+		echo "	{ 0, 0 }"
+}
+
 # Print into the standart output the list of MySQL codes for errors
 get_error_codes(){
-	echo $MYSQL_PATH;
+	generate_error_code_c_chunk > "$TMP_PATH/mysql_errmsg.h"
+	cp src-in/mysql_generr.c "$TMP_PATH/"
+	$CC "${TMP_PATH}/mysql_generr.c" -o "${TMP_PATH}/mysql_generr" $MYSQL_CFLAGS $MYSQL_LIBS -I"${TMP_PATH}" && ./"$TMP_PATH/mysql_generr"
 }
 
 
